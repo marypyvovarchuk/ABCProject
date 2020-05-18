49a5a314b4c9d98a97f02e30138e0989ef7aea66
Mary Pyvovarchuk <pyvovarchukmaria@gmail.com>
2020-05-18 09:31:39 +0300

    Added first library

---
+++ library1/src/test/kotlin/com/ABCProject/library1/Library1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ABCProject.library1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Library1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
